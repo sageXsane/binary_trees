97131a404fd5221f2a00de3b18b0367a08f52045
sagexsane <sanelisiwebhengu01@gmail.com>
2024-06-06 00:13:11 +0200

    Add func description task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,6 +1,11 @@
 #include "binary_trees.h"
 
-
+/**
+ * binary_tree_size - determine number of nodes in/ size of bt
+ * @tree: pointer to root node
+ *
+ * Return: 0 if tree empty else number of nodes
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t sizeLeft, sizeRight;
