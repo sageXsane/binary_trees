013e1fa1469d95fffafbb7ae24689e61c69dac99
sagexsane <sanelisiwebhengu01@gmail.com>
2024-06-06 23:24:35 +0200

    Change task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,69 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - checks whether subtree/tree is perfect BT
- * @tree: pointer to root of tree/subtree
+ * binary_tree_is_perfect - determines whether perfect tree.
+ * @tree: pointer of root node
  *
+ * Description: compares number of nodes to the number of nodes a perfect tree
+ * with same height would have
  * Return: 1 if perfect else 0
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int balance, full;
+	int num_nodes = 0;
+	int height = 0;
+	int perfect_nodes;
 
 	if (tree == NULL)
 		return (0);
-
-	balance = binary_tree_balance(tree);
-	full = binary_tree_is_full(tree);
-
-	if ((balance == 0) && (full == 1))
-		return (1);
-	else
-		return (0);
+	num_nodes = (int)binary_tree_nodes(tree);
+	height = (int)binary_tree_height(tree);
+	perfect_nodes = (1 << (height + 1)) - 1;
+	return (perfect_nodes == num_nodes);
 }
 
 /**
- * binary_tree_is_full - checks if BT is a full BT
- * @tree: pointer to root node
- *
- * Return: 0 if tree is empty or node has one child else 1
- */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	int fullLeft, fullRight;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		if ((tree->right == NULL) && (tree->left == NULL))
-		{
-			return (1);
-		}
-		else if ((tree->right != NULL) && (tree->left != NULL))
-		{
-			fullRight = binary_tree_is_full(tree->right);
-			fullLeft = binary_tree_is_full(tree->left);
-			return (fullRight && fullLeft);
-		}
-		else
-		{
-			return (0);
-		}
-	}
-}
-
-/**
- * binary_tree_height_2 - determines the height of a bt
+ * binary_tree_height - determines the height of a bt
  * @tree: pointer of root node of subtree
  *
  * Return: height of bt else 0 if tree is empty or if has only root
  */
-int binary_tree_height_2(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int heightLeft, heightRight, height;
+	size_t heightLeft, heightRight, height;
 
 	if (tree == NULL)
 	{
@@ -71,31 +38,27 @@ int binary_tree_height_2(const binary_tree_t *tree)
 	}
 	else
 	{
-		heightLeft = binary_tree_height_2(tree->left);
-		heightRight = binary_tree_height_2(tree->right);
+		heightLeft = binary_tree_height(tree->left);
+		heightRight = binary_tree_height(tree->right);
 		height = (heightLeft > heightRight) ? heightLeft : heightRight;
-		return (height + 1);
+
+		if ((tree->left == NULL) && (tree->right == NULL))
+			return (height);
+		else
+			return (height + 1);
 	}
 }
 
 /**
- * binary_tree_balance - calculates the balance of bt with root node tree
- * @tree: pointer to node of subtree
+ * binary_tree_nodes - counts the number of leaves a bt has
+ * @tree: pointer to the root node of the tree to count the number of nodes
  *
- * Return: return 0 when tree is empty else left and right height
+ * Return: return number of child having nodes or 0 if tree is empty
  */
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	int heightLeft, heightRight;
-
 	if (tree == NULL)
-	{
 		return (0);
-	}
 	else
-	{
-		heightLeft = binary_tree_height_2(tree->left);
-		heightRight = binary_tree_height_2(tree->right);
-		return (heightLeft - heightRight);
-	}
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 }
